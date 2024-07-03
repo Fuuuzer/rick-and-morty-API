806b41c4455145c4eaf44612ef2ca1fca95b8bb5
Fuzer <fuuzer_@hotmail.com>
2024-07-03 19:08:44 -0300

    title css, button added

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const container = document.querySelector('.container')
-const charactersCount = 20;
+const container = document.querySelector(".container");
+const charactersCount = 21;
 const colorDeadOrAlive = {
   alive: "#30be30",
-  dead: "#bb2323"
-}
+  dead: "#bb2323",
+};
 
-const mainStatus = Object.keys(colorDeadOrAlive)
+const mainStatus = Object.keys(colorDeadOrAlive);
 
 const fetchCharacters = async () => {
   for (let i = 1; i <= charactersCount; i++) {
-    await getCharacters(i)
+    await getCharacters(i);
   }
-}
+};
 
 console.log(mainStatus);
-
 
 const getCharacters = async (id) => {
   const url = `https://rickandmortyapi.com/api/character/${id}`;
   const resp = await fetch(url);
   const data = await resp.json();
 
-  createCharacterCard(data)
-}
+  createCharacterCard(data);
+};
 
 const createCharacterCard = (character) => {
-  const content = document.createElement('div');
+  const content = document.createElement("div");
   const deadOrAlive = character.status.toLowerCase();
-  
+
   content.innerHTML = `<div class="character__card">
   <img src="https://rickandmortyapi.com/api/character/avatar/${character.id}.jpeg" alt="">
   <div class="character__info">
@@ -40,11 +39,8 @@ const createCharacterCard = (character) => {
       <p>${character.location.name}</p>
     </div>
   </div>
-</div>`
-container.appendChild(content)
-
-}
-
+</div>`;
+  container.appendChild(content);
+};
 
 fetchCharacters();
-
