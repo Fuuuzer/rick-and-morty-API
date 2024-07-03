f2c4b9259fdc03fc8e3c788c610bcc91df950721
Fuzer <fuuzer.@hotmail.com>
2024-07-03 14:04:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const container = document.querySelector('.container')
+const charactersCount = 20;
+const colorDeadOrAlive = {
+  alive: "#30be30",
+  dead: "#bb2323"
+}
+
+const mainStatus = Object.keys(colorDeadOrAlive)
+
+const fetchCharacters = async () => {
+  for (let i = 1; i <= charactersCount; i++) {
+    await getCharacters(i)
+  }
+}
+
+console.log(mainStatus);
+
+
+const getCharacters = async (id) => {
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+
+  createCharacterCard(data)
+}
+
+const createCharacterCard = (character) => {
+  const content = document.createElement('div');
+  const deadOrAlive = character.status.toLowerCase();
+  
+  content.innerHTML = `<div class="character__card">
+  <img src="https://rickandmortyapi.com/api/character/avatar/${character.id}.jpeg" alt="">
+  <div class="character__info">
+    <div class="info__basics">
+      <h2>${character.name}</h2>
+      <span class="live_or_dead ${deadOrAlive}">${character.status} - ${character.species}</span>
+    </div>
+    <div class="info__location">
+      <p>Last known location:</p>
+      <p>${character.location.name}</p>
+    </div>
+  </div>
+</div>`
+container.appendChild(content)
+
+}
+
+
+fetchCharacters();
+
